ad84363ce9b3b78b135b3bfe78f4ba8227c49a8c
heba.edawi <heba.eldawi@gmail.com>
2018-11-12 09:29:18 +0200

    adding build folder

---
+++ build/precache-manifest.5356517ba243b1af0c3b33bdc5b76c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d026bc8b9c4be186a9",
+    "url": "/static/css/main.c37c5bdf.chunk.css"
+  },
+  {
+    "revision": "02d026bc8b9c4be186a9",
+    "url": "/static/js/main.02d026bc.chunk.js"
+  },
+  {
+    "revision": "9890fc6218454330d3b8",
+    "url": "/static/js/1.9890fc62.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b14ed594792e73e8ddae22c3eb9d88c6",
+    "url": "/static/media/logo.b14ed594.svg"
+  },
+  {
+    "revision": "d2a8903e7bb720a592b0f0b595ee961d",
+    "url": "/index.html"
+  }
+];
